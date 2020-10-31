f66e7d130f55afa783ab82010da8c66a531a7524
呂冠慶 <charlie@lvguanqingdeMacBook-Pro.local>
2020-10-31 23:30:28 +0800

    建立新檔案

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'standard',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'import/extensions': ['error'],
+  },
+};
